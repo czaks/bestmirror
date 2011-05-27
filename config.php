39e74b48f73f8ee74e997f059b3c50c4fef3b13e
czaks <marcin@6irc.net>
2011-05-27 13:34:55 +0200

    6IRCNet specific: Add zlew to default config

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 // bestmirror config
 
 $mirrors = array(
-	"http://aus.webchat.6irc.net" => "http://aus.webchat.6irc.net/images/qwebircsmall.png",
+	"http://zlew.webchat.6irc.net" => "http://zlew.webchat.6irc.net/images/qwebircsmall.png",
 	"http://sundance.webchat.6irc.net" => "http://sundance.webchat.6irc.net/images/qwebircsmall.png",
 	"http://minimal.webchat.6irc.net" => "http://minimal.webchat.6irc.net/images/qwebircsmall.png",
 );
